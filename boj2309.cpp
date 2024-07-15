a61db667a637c98b197f2281f56f9e5fc1f1067f
todus26 <jungsae2248@naver.com>
2024-07-15 20:37:38 +0900

    cpp_boj_0715

---
+++ boj2309.cpp
@@ -0,0 +1,25 @@
+// 2309번 일곱 난쟁이
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[9];
+    int sum = 0;
+    for (int i = 0; i < 9; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    sort(arr, arr + 9);
+    for (int i = 0; i < 9; i++) {
+        for (int j = i + 1; j < 9; j++) {
+            if (sum - arr[i] - arr[j] == 100) {
+                for (int k = 0; k < 9; k++) {
+                    if (k == i || k == j) continue;
+                    cout << arr[k] << endl;
+                }
+                return 0;
+            }
+        }
+    }
+}
